f03dfe5accdfe59d2699281c1cd81988f80f2af6
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-08-10 15:00:30 +0200

    get local storage

--- cinema-seat-reservation-app/script.js
+++ cinema-seat-reservation-app/script.js
@@ -7,6 +7,9 @@ const seats = document.querySelectorAll(
   ".seat:not(.reserved, .select-info, .empty)"
 );
 
+getFromLocalStorage();
+calculateTotal();
+
 container.addEventListener("click", function (e) {
   if (
     e.target.classList.contains("seat") &&
@@ -57,6 +60,21 @@ function saveToLocalStorage(index) {
   localStorage.setItem("selectedMovieIndex", select.selectedIndex);
 }
 
+function getFromLocalStorage() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats != null && selectedSeats.length > 0) {
+    seats.forEach(function (seat, index) {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex != null) {
+    select.selectedIndex = selectedMovieIndex;
+  }
+}
 function showAlert() {
   let alert = document.getElementById("snackbar");
   alert.className = "show";
