4ba86ff139992bba491da5eb8adb75e81f874cb6
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-08-17 17:59:02 +0200

    quiz constructor

--- oop-quiz-app/script.js
+++ oop-quiz-app/script.js
@@ -8,6 +8,34 @@ function Question(text, choices, answer) {
 Question.prototype.checkAnswer = function (answer) {
   return this.answer === answer;
 };
+
+//Quiz constructor
+function Quiz(questions) {
+  this.questions = questions;
+  this.score = 0;
+  this.questionsIndex = 0;
+}
+
+//Quiz Prototype
+Quiz.prototype.getQuestion = function () {
+  return this.questions[this.questionsIndex];
+};
+
+//Quiz isFinish
+Quiz.prototype.isFinish = function () {
+  return this.questions.length === this.questionsIndex;
+};
+
+//Quiz guess {
+Quiz.prototype.guess = function (answer) {
+  let questions = this.getQuestion();
+
+  if (questions.checkAnswer(answer)) {
+    this.score++;
+  }
+  this.questionsIndex++;
+};
+
 let q1 = new Question(
   "What's the best programming?",
   ["C#", "JavaScript", "Python", "Asp.Net"],
@@ -24,8 +52,20 @@ let q3 = new Question(
   "JavaScript"
 );
 
-console.log(q1.checkAnswer("C#"));
-console.log(q1.checkAnswer("JavaScript"));
+let questions = [q1, q2, q3];
 
-console.log(q2.checkAnswer("Visual Basic"));
-console.log(q2.checkAnswer("JavaScript"));
+// Start Quiz
+
+let quiz = new Quiz(questions);
+
+console.log(quiz.isFinish());
+
+console.log(quiz.getQuestion());
+quiz.guess('JavaScript')
+console.log(quiz.getQuestion());
+quiz.guess('JavaScript')
+console.log(quiz.getQuestion());
+quiz.guess('JavaScript')
+
+console.log(quiz.score)
+console.log(quiz.isFinish());
