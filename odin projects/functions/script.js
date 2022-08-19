cc5f1c8a826a552f4121094d31e64fab36fe9a40
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-08-19 12:39:31 +0200

    functions

---
+++ odin projects/functions/script.js
@@ -0,0 +1,24 @@
+function add7(num) {
+  return num + 7;
+}
+
+console.log(add7(5));
+
+function multiply(y, x) {
+  return y * x;
+}
+
+console.log(multiply(5, 7));
+
+function capitalize(str) {
+  return str.slice(0,1).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+console.log(capitalize("pencil"));
+
+
+function lastletter(str) {
+ return str.slice(length - 1)
+}
+
+console.log(lastletter("abcd"))
