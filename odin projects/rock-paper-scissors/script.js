ceebc255fda4ef1427019f24d6baa95f30e5c8a5
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-08-21 17:57:32 +0200

    game

--- odin projects/rock-paper-scissors/script.js
+++ odin projects/rock-paper-scissors/script.js
@@ -1,26 +1,55 @@
 function getComputerChoice() {
-  let computerChoices = ["rock", "paper", "scissors"];
-  return computerChoices[Math.floor(Math.random() * 3)];
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
-// console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection) {
   let playerPoint = 0;
-
-  if (
+  if (playerSelection == computerSelection) {
+    console.log(`It's a tie. You both chose ${playerSelection}`);
+  } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
     playerPoint = 1;
-    console.log(`'You won! ${playerSelection} beats ${computerSelection}`);
+    console.log(`You won! ${playerSelection} beats ${computerSelection}`);
   } else {
     console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
   }
   return playerPoint;
 }
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
 
-// console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerScore = 0;
+  let roundsPlayed = 0;
+  let computerSelection = getComputerChoice();
+  for (let i = 0; i < 5; i++) {
+    playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase().trim();
+    if (
+      playerSelection != "rock" &&
+      playerSelection != "paper" &&
+      playerSelection != "scissors"
+    ) {
+      continue;
+    }
+    playerScore += playRound(playerSelection, computerSelection);
+    roundsPlayed++;
+  }
 
+  if (playerScore > roundsPlayed / 2) {
+    console.log(
+      `You won ${playerScore} out of ${roundsPlayed} rounds. You win!`
+    );
+  } else if (playerScore == roundsPlayed / 2) {
+    console.log(
+      `You won ${playerScore} out of ${roundsPlayed} rounds. You tie.`
+    );
+  } else {
+    console.log(
+      `You won ${playerScore} out of ${roundsPlayed} rounds.You lost!`
+    );
+  }
+}
+
+// game();
