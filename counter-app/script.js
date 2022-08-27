dd603bd2da08b9505ec30fb8722d8782808a2ccf
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-08-27 21:31:28 +0200

    add new functions

--- counter-app/script.js
+++ counter-app/script.js
@@ -6,35 +6,19 @@ let count = 0;
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const styles = e.currentTarget.classList;
-
     if (styles.contains("add")) {
-      count++;
+      if (counter.textContent >= 10) {
+        counter.textContent = 0;
+      } else {
+        count++;
+      }
     } else if (styles.contains("subtract")) {
-      count--;
+      if (counter.textContent <= 0) {
+        counter.textContent = 0;
+      } else {
+        count--;
+      }
     }
-
-    if (count > 10) {
-      counter.textContent = 0;
-    } else if (count < 0) {
-      counter.textContent = 0;
-    } else {
-      counter.textContent = count;
-    }
+    counter.textContent = count;
   });
 });
-
-// add.addEventListener("click", function () {
-//   let output = document.querySelector("#output");
-//   output.innerText++;
-//   if (output.innerText > 10) {
-//     output.innerText = 0;
-//   }
-// });
-
-// subtract.addEventListener("click", function () {
-//  let output = document.querySelector("#output");
-//  output.innerText--;
-//  if(output.innerText < 0) {
-//   output.innerText = 0;
-//  }
-// })
